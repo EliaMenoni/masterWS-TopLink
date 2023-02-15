d1894c6d923d8daff7ac377257b983dd81b0f1cc
Elia Menoni <menonielia00@gmail.com>
2023-02-15 12:53:44 +0100

    Revisione struttura file XML per ClinicalDocument

---
+++ LDO/tools.py
@@ -0,0 +1,2 @@
+
+def recParseXML(XMLRoot, FolderRoot):
--- LDO/views.py
+++ LDO/views.py
@@ -4,7 +4,5 @@ import xml.etree.ElementTree as ET
 
 # Create your views here.
 def LDOXMLCreator(request, id):
-    root = ET.Element("ClinicalDocument" + str(id))
-    root.attrib['schemaLocation'] = "BLA BLA BLA BL"
-    ET.SubElement(root, 'PROVA', {'arrt': 'ciaociao'}).text = "domani"
+    
     return HttpResponse(ET.tostring(root), content_type="application/xml")
